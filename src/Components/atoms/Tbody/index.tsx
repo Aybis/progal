40a56551a805a8e52b5c04dedc2a6f651bf1ae0a
Aybis <muchtara2m@gmail.com>
2022-12-11 08:17:32 +0700

    tbody

--- src/Components/atoms/Tbody/index.tsx
+++ src/Components/atoms/Tbody/index.tsx
@@ -13,7 +13,7 @@ export default function Index(props: TbodyProps) {
       rowSpan={props.rowSpan}
       colSpan={props.colSpan}
       className={[
-        'p-4 relative text-sm leading-relaxed font-medium text-gray-800',
+        'p-4 relative text-sm leading-relaxed font-medium text-gray-800 border-l',
         props.className,
       ].join(' ')}>
       {props.children}
